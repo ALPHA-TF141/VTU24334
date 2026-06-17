914275cdf9c3550d9cefb08fa51219e4a3ce33c5
ALPHA-TF141 <lmariaimmanuel@gmail.com>
2026-06-17 12:56:22 +0530

    Refactor notification fetching logic and update useNotifications hook to handle pagination and filtering

--- Campus-Evaluation-FS/notification-app-fe/src/api/notifications.js
+++ Campus-Evaluation-FS/notification-app-fe/src/api/notifications.js
@@ -1,2 +1,30 @@
-export function fetchNotifications() {
+import axios from "axios";
+
+const API_URL =
+  "http://4.224.186.213/evaluation-service/notifications";
+
+const TOKEN = import.meta.env.VITE_TOKEN;
+
+export async function fetchNotifications(
+  page = 1,
+  limit = 20,
+  type = "All"
+) {
+  const params = {
+    page,
+    limit,
+  };
+
+  if (type !== "All") {
+    params.notification_type = type;
+  }
+
+  const response = await axios.get(API_URL, {
+    params,
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  return response.data;
 }
